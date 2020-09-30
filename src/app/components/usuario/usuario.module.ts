bdb14f1f9f46fd0f9dbdd5ead415a4d3bd5a72ae
Leonardo Oliveira <leonardo.oliveira@basis.com.br>
2020-09-30 12:56:27 -0300

    Ajuste componente usuarios

--- src/app/components/usuario/usuario.module.ts
+++ src/app/components/usuario/usuario.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UsuarioRoute } from './usuario.route';
+import { PRIMENG_IMPORTS } from 'src/app/primeng-imports';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { UsuarioComponent } from './usuario.component';
 
 
 let ENTITY_STATES = [
@@ -9,10 +13,14 @@ let ENTITY_STATES = [
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [UsuarioComponent],
   imports: [
     CommonModule,
     RouterModule.forRoot( ENTITY_STATES, { useHash: true } ),
-  ]
+    PRIMENG_IMPORTS,
+    FormsModule,
+    BrowserModule,
+  ],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class UsuarioModule { }
